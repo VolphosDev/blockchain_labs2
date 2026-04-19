339db6b0920bb8696fe66849b40e3e13d94d7da4
VolphosDev <moralesbenitescharlie@gmail.com>
2026-04-18 22:47:25 -0500

    first-commit

---
+++ contracts/1_Storage.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2 <0.9.0;
+
+/**
+ * @title Storage
+ * @dev Store & retrieve value in a variable
+ * @custom:dev-run-script ./scripts/deploy_with_ethers.ts
+ */
+contract Storage {
+
+    uint256 number; //este e mi comentario de una sola línea
+
+    /*
+    Este es un comentario 
+    multi
+    línea
+     */
+    function store(uint256 num) public {
+        number = num;
+    }
+
+    /**
+     * @dev Return value 
+     * @return value of 'number'
+     */
+    function retrieve() public view returns (uint256){
+        return number;
+    }
+
+    function devolver() public view returns (uint256){
+        return number;
+    }
+}
