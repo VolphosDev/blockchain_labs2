7a173aa1767627594aaf56f5e9257ee5b3a2583f
VolphosDev <moralesbenitescharlie@gmail.com>
2026-05-16 20:27:08 -0500

    Parte 2

--- contracts/Ferreteria000256897.sol
+++ contracts/Ferreteria000256897.sol
@@ -1,6 +1,14 @@
 pragma solidity >=0.8.2 <0.9.0;
 
+import "hardhat/console.sol";
+
 contract Ferreteria000256897 {
+
+    address public dirContrato = 0x7b96aF9Bd211cBf6BA5b0dd53aa61Dc5806b6AcE;
+
+    constructor() {
+        console.log("Ejecutado por: 000256897 - Morales Benites Charlie");
+    }
 
     struct Herramienta {
         uint256 id;
@@ -9,7 +17,12 @@ contract Ferreteria000256897 {
     }
     Herramienta[] public inventario;
 
-    function agregarHerramienta(uint256 _id, string memory _nombre, uint256 _cantidad) public {
+    function agregarElemento(uint256 _id, string memory _nombre, uint256 _cantidad) public {
         inventario.push(Herramienta(_id, _nombre, _cantidad));
+    }
+
+    function contarElementos() public view returns (uint256) {
+        console.log("Ejecutado por: 000256897 - Morales Benites Charlie");
+        return inventario.length;
     }
 }
